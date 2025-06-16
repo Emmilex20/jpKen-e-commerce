c43b289bb0b46c54c30f17ee79e0e91447cdfb63
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2025-06-16 14:32:18 +0100

    second commit 14

--- node_modules/.pnpm/node_modules/server/src/utils/generateToken.js
+++ node_modules/.pnpm/node_modules/server/src/utils/generateToken.js
@@ -9,8 +9,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: isProduction, // TRUE IN PRODUCTION (HTTPS)
-    sameSite: isProduction ? 'None' : 'strict', // 'None' FOR CROSS-ORIGIN IN PRODUCTION
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'None', // 'None' FOR CROSS-ORIGIN IN PRODUCTION
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
